f02293f5d8bf256ebbdd4f39ad95c19b89d0ada5
jaklin99 <jaklin.yanakieva@gmail.com>
2025-05-20 16:11:29 +0200

    adjustments to the index/script and style so that the card looks nice and has functionalities

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-function revealSurprise() {
-  document.getElementById("surprise").classList.remove("hidden");
+// function revealSurprise() {
+//   document.getElementById("surprise").classList.remove("hidden");
 
-  // Confetti animation
-  const duration = 6 * 1000;
+//   // Confetti animation
+//   const duration = 6 * 1000;
+//   const end = Date.now() + duration;
+
+//   (function frame() {
+//     confetti({
+//       particleCount: 6,
+//       angle: 60,
+//       spread: 70,
+//       origin: { x: 0 },
+//     });
+//     confetti({
+//       particleCount: 6,
+//       angle: 120,
+//       spread: 70,
+//       origin: { x: 1 },
+//     });
+
+//     if (Date.now() < end) {
+//       requestAnimationFrame(frame);
+//     }
+//   })();
+// }
+
+// function showPlan() {
+//   document.getElementById("trip-plan").classList.remove("hidden");
+// }
+
+function revealSurprise() {
+  document.getElementById("card2").classList.remove("hidden");
+
+  const duration = 3 * 1000;
   const end = Date.now() + duration;
 
   (function frame() {
     confetti({
-      particleCount: 5,
+      particleCount: 6,
       angle: 60,
       spread: 70,
       origin: { x: 0 },
     });
     confetti({
-      particleCount: 5,
+      particleCount: 6,
       angle: 120,
       spread: 70,
       origin: { x: 1 },
@@ -23,4 +53,8 @@ function revealSurprise() {
       requestAnimationFrame(frame);
     }
   })();
+}
+
+function showPlan() {
+  document.getElementById("trip-plan").classList.remove("hidden");
 }
